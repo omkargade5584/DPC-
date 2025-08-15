a1af304cafa882c3ace4f8ed5de74f305a4c14e3
omkargade5584 <omkargade2804@gmail.com>
2025-08-15 12:54:11 +0530

    Create Day1.java
    Day 1 challenge

---
+++ Day1.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Day1 {
+    public static void sortColors(int[] arr) {
+        int low = 0, mid = 0, high = arr.length - 1;
+
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else { 
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2, 1, 0, 2, 1, 0};
+        sortColors(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
